ba986f8fdfa5daac4cc0a436047915c115837523
Dejan Francuz <dejanfrancuz508@gmail.com>
2023-10-23 19:21:00 +0200

    added contact form

--- pages/Contact.py
+++ pages/Contact.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import os
 
 st.set_page_config(
     page_title="Contact",
     page_icon="📫",
 )
+
+st.title('Contact Form')
+
+formsubmit_url = os.environ.get("FORMSUBMIT_URL")
+
+contact_form = f"""
+<form action="{formsubmit_url}" method="POST">
+     <input type="text" name="name" placeholder="Full Name" required>
+     <input type="email" name="email" placeholder="Email Address" required>
+     <textarea name="message" placeholder="Your message"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
