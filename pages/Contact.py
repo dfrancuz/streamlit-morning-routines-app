759df728808b6fb2463b0b68191c4988abc1e36c
Dejan Francuz <dejanfrancuz508@gmail.com>
2023-10-23 19:47:02 +0200

    contact form redesigned

--- pages/Contact.py
+++ pages/Contact.py
@@ -20,3 +20,9 @@ contact_form = f"""
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+local_css("style/style.css")
